a6487e62802fc8300060912e22e19d974eb56ada
dididiri1 <dididiri1@naver.com>
2023-06-22 15:58:10 +0900

    예제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,44 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
-@Getter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
 
-    protected Member() {
-    }
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            this.team = team;
+        }
+    }
 
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
--- study/01_spring_data_jpa.md
+++ study/01_spring_data_jpa.md
@@ -8,11 +8,28 @@
  ./gradlew dependencies --configuration compileClasspath
 ``` 
 
-![](https://github.com/dididiri1/jpashop/blob/main/study/images/02_04.png?raw=true)
+### 쿼리 파라미터 로그 남기기
 
-### 스프링 데이터 JPA와 DB 설정, 동작확인
-
-- application.yml
+- 로그에 다음을 추가하기 org.hibernate.type : SQL 실행 파라미터를 로그로 남긴다.
+- 외부 라이브러리 사용
 ``` yml
-  
+  implementation 'com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.5.6'
 ``` 
+
+> 참고: 쿼리 파라미터를 로그로 남기는 외부 라이브러리는 시스템 자원을 사용하므로, 개발 단계에서는 편하게 사용해도 된다.
+> 하지만 운영시스템에 적용하려면 꼭 성능테스트를 하고 사용하는 것이 좋다.
+
+## 예제 도메인 모델
+
+### 예제 도메인 모델과 동작확인
+
+![](https://github.com/dididiri1/data-jpa/blob/main/study/images/01_01.png?raw=true)
+
+
+- ERD
+![](https://github.com/dididiri1/data-jpa/blob/main/study/images/01_02.png?raw=true)
+
+
+- 꿀팁 (JAP 엔티티 빨간줄 해줌)
+
+![](https://github.com/dididiri1/data-jpa/blob/main/study/images/01_03.png?raw=true)
